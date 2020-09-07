7f641e8f1c144b3228fd6b86199cc3d2c7522de8
Gretchen Shelby-Dormer <gshelby@coveo.com>
2020-09-07 15:16:20 -0400

    Make issue.log() functional
    * Sort imports.
    * Fix config.instantiate_jira_from_config typo.
    * Add issue.get().
    * Return information about a ticket if `jira-cli issue COMPANY TICKET`.
    * Use jira.issue_add_json_worklog(), allowing for the removal of
    `get_equivalent_seconds()` and `MEASURE_IN_SEC`.
    * Add sphinx docstrings, include `\f` to prevent click from including in `--help`.
    * Change issue.log() param `started` from argument to option. If not provided, Jira will
    use current time.

--- jiracli/commands/issue.py
+++ jiracli/commands/issue.py
@@ -1,57 +1,73 @@
-import click
-import logging
 import configparser
-from pprint import pprint
+import logging
 import re
+from datetime import datetime
+from pprint import pprint
+
+import click
 import requests
 from atlassian import jira
 
-from jiracli.commands.config import instanciate_jira_from_config
+from jiracli.commands.config import instantiate_jira_from_config
+
+DATETIME_FORMAT = '%m/%d/%y %H:%M'
 
 
-MEASURE_IN_SEC = {"w":604800, "d":86400, "h":3600, "m":60}
-
-
-def get_equivalent_seconds(time_str: str):
-    total_seconds = 0
-    for i in time_str.split(' '):
-        number = int(re.findall("\d+", i)[0])
-        measure = re.findall("\D+", i)[0]
-        if measure not in MEASURE_IN_SEC.keys():
-            raise ValueError(f"Measurement of time must be one of {list(MEASURE_IN_SEC.keys())}; invalid: {measure} in {i}")
-        else:
-            total_seconds += number * MEASURE_IN_SEC[measure]
-    return total_seconds
-
-
-@click.group()
+@click.group(invoke_without_command=True)
 @click.argument("company")
 @click.argument("ticket")
 @click.pass_context
 def issue(ctx, company: str, ticket: str):
-    jira = instanciate_jira_from_config(company=company)
+    """Return basic information about a ticket; subcommands also available.
+    \f
+    :param company: Must be one of the companies configured in ``.jira-cli/config``.
+    :type company: str
+    :param ticket: Issue key, e.g. PROJECT-###.
+    :type ticket: str
+    """
+    jira = instantiate_jira_from_config(company=company)
     ctx.ensure_object(dict)
     ctx.obj['company'] = company
     ctx.obj['ticket'] = ticket
     ctx.obj['jira'] = jira
 
+    if ctx.invoked_subcommand is None:
+        get(ctx)
+
+
 @issue.command()
+@click.option("-s", "--started")
 @click.option("-c", "--comment")
-@click.argument("STARTED")
 @click.argument("TIME")
 @click.pass_context
-def log(ctx, started: str, time: str, comment: str):
-    """Log time into a ticket."""
-    total_seconds = get_equivalent_seconds(time)
-    worklog = {
-        "started": started,
-        "timeSpent": time,
-    }
+def log(ctx, time: str, started: str = None, comment: str = None):
+    """Log time into a ticket.
+    \f
+    :param time: Amount of time spent, e.g. ``2w 4d 6h 45m``.
+    :type time: str
+    :param started: Time started at, format ``%m/%d/%y %H:%M``.
+    :type started: str
+    :param comment: Comment to include in worklog.
+    :type comment: str
+    """
+    worklog = {"timeSpent": time}
+    if started:
+        started_datetime = datetime.strptime(started, DATETIME_FORMAT)
+        worklog["started"] = started_datetime.strftime("%Y-%m-%dT%H:%M:%S.000+0000%z")
     if comment:
         worklog["comment"] = comment
     jira = ctx.obj['jira']
-    # jira.issue_worklog(key=ctx.obj['ticket'], started=started, time_sec=total_seconds, comment=comment)
-    response = jira.issue_add_json_worklog(key=ctx.obj['ticket'], worklog=worklog)
-    # response = jira.issue_get_worklog(issue_id_or_key=ctx.obj['ticket'])
-    pprint(response)
-    # logging.info(f"""Logging {time} ({total_seconds}s) into {ctx.obj['company']} {ctx.obj['ticket']} {f"with comment: {comment}" if comment else ""}""")
+    jira.issue_add_json_worklog(key=ctx.obj['ticket'], worklog=worklog)
+
+
+def get(ctx):
+    """Return information about a ticket."""
+    jira = ctx.obj['jira']
+    returned_information = jira.issue(key=ctx.obj['ticket'])
+    click.echo()
+    click.echo(f"Title:    {returned_information['fields']['summary']}")
+    click.echo(f"Reporter: {returned_information['fields']['reporter']['displayName']}")
+    click.echo(f"Assignee: {returned_information['fields']['assignee']['displayName']}")
+    click.echo(f"Status:   {returned_information['fields']['status']['name']}")
+    click.echo(f"URL:      {ctx.obj['jira'].url}/browse/{ctx.obj['ticket']}")
+    click.echo()
